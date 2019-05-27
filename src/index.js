35b8ac44bc0baaae4d4608f3aa2a77b0d1608e5b
erenude <anahita.vahdani@neuf.fr>
2019-05-27 23:40:03 +0200

    react new features

---
+++ src/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import * as serviceWorker from './serviceWorker';
+import NoteApp from './components/NoteApp'
+
+ReactDOM.render(<NoteApp />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
+
+
+
+// const App = (props) => {
+//   const [count, setCount] = useState(props.count)
+//   const [text, setText] = useState('')
+
+//   useEffect(() => {
+//     console.log('This should only run once!')
+//   }, [])
+
+//   useEffect(() => {
+//     console.log('useEffect ran')
+//     document.title = count
+//   }, [count])
+
+//   // const increment = () => {
+//   //   setCount(count +1)
+//   // }
+
+//   return (
+//     <div>
+//       <p>The current {text || 'count'} is {count}</p>
+//       <button onClick={() => setCount(count -1)}>-1</button>
+//       <button onClick={() => setCount(props.count)}>reset</button>
+//       <button onClick={() => setCount(count +1)}>+1</button>
+//       <input value={text} onChange={(e) => setText(e.target.value)}/>
+//     </div>
+//   )
+// }
